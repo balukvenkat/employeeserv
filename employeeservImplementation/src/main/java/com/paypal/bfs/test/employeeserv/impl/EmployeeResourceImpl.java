29715aa23f54f543afaedeac16ec7d7b22eebd67
bk <38793410+balukvenkat@users.noreply.github.com>
2020-07-19 00:25:43 -0400

    first commit

---
+++ employeeservImplementation/src/main/java/com/paypal/bfs/test/employeeserv/impl/EmployeeResourceImpl.java
@@ -0,0 +1,84 @@
+package com.paypal.bfs.test.employeeserv.impl;
+
+import com.paypal.bfs.test.employeeserv.api.EmployeeResource;
+import com.paypal.bfs.test.employeeserv.api.model.Employee;
+import com.paypal.bfs.test.employeeserv.dao.EmployeeRepository;
+import com.paypal.bfs.test.employeeserv.findings.Errors;
+import com.paypal.bfs.test.employeeserv.mapper.EmployeeMapper;
+import com.paypal.bfs.test.employeeserv.model.EmployeeTable;
+import com.paypal.bfs.test.employeeserv.util.InputValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Implementation class for employee resource.
+ */
+@RestController
+public class EmployeeResourceImpl implements EmployeeResource {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private EmployeeMapper employeeMapper;
+
+    @Autowired
+    private InputValidator inputValidator;
+
+    private EmployeeResourceImpl(EmployeeRepository employeeRepository, EmployeeMapper employeeMapper, InputValidator inputValidator) {
+        this.employeeRepository = employeeRepository;
+        this.employeeMapper = employeeMapper;
+        this.inputValidator = inputValidator;
+    }
+
+    @Override
+    public ResponseEntity<Employee> employeeGetById(String id) {
+
+        Optional<EmployeeTable> et = employeeRepository.findById(Integer.valueOf(id));
+
+        if(et.isPresent()){
+            return new ResponseEntity<>(employeeMapper.forAPI(et.get()),HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @Override
+    public ResponseEntity createEmployee(Employee employeeRequest) {
+       // final String errors = getErrors(employeeRequest);
+        if(null != employeeRequest.getId() ){
+            Optional<EmployeeTable> et = employeeRepository.findById(employeeRequest.getId());
+            if(et.isPresent()){
+                return ResponseEntity
+                        .status(HttpStatus.FORBIDDEN)
+                        .body("Employee Exists!");
+            }
+        }
+
+        Optional<List<Errors>> error = inputValidator.getErrors(employeeRequest);
+        if(error.isPresent()){
+            return ResponseEntity
+                    .status(HttpStatus.FORBIDDEN)
+                    .body(error);
+        }
+
+        try{
+
+             employeeRepository.save(employeeMapper.forDB(employeeRequest));
+            return new ResponseEntity<>(HttpStatus.CREATED);
+
+
+        }catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.EXPECTATION_FAILED);
+        }
+
+    }
+
+
+
+}
---
+++ employeeservImplementation/src/main/java/com/paypal/bfs/test/employeeserv/util/InputValidator.java
@@ -0,0 +1,85 @@
+package com.paypal.bfs.test.employeeserv.util;
+
+
+import com.paypal.bfs.test.employeeserv.api.model.Employee;
+import com.paypal.bfs.test.employeeserv.findings.Errors;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Component
+public class InputValidator {
+
+    private static final String FIRST_NAME = "First Name";
+    private static final String LAST_NAME = "Last Name";
+    private static final String DATE_OF_BIRTH = "Date of Birth";
+    private static final String ADDRESS_LINE_1 = "Address Line 1";
+    private static final String CITY = "city";
+    private static final String STATE = "state";
+    private static final String ZIP_CODE = "zip code";
+    private static final String COUNTRY = "country";
+
+    private static final String REQUIRED = "This is required";
+
+    public Optional<List<Errors>> getErrors(Employee employeeRequest) {
+        List<Errors> errorsList = new ArrayList<>();
+
+
+
+        checkForRequired(employeeRequest, errorsList);
+
+
+
+        //max length
+
+        //Date format
+
+        if(errorsList.size()>0){
+            return Optional.of(errorsList);
+        }
+
+        return Optional.empty();
+    }
+
+    private void checkForRequired(Employee employeeRequest, List<Errors> errorsList) {
+        //last name
+        //first name
+        if(isEmpty(employeeRequest.getFirstName())){
+            errorsList.add(Errors.builder().field(FIRST_NAME).message(REQUIRED).build());
+        }
+
+        if(isEmpty(employeeRequest.getLastName())){
+            errorsList.add(Errors.builder().field(LAST_NAME).message(REQUIRED).build());
+        }
+
+        if(isEmpty(employeeRequest.getDateOfBirth())){
+            errorsList.add(Errors.builder().field(DATE_OF_BIRTH).message(REQUIRED).build());
+        }
+
+        if(isEmpty(employeeRequest.getAddressLine1())){
+            errorsList.add(Errors.builder().field(ADDRESS_LINE_1).message(REQUIRED).build());
+        }
+
+        if(isEmpty(employeeRequest.getCity())){
+            errorsList.add(Errors.builder().field(CITY).message(REQUIRED).build());
+        }
+        if(isEmpty(employeeRequest.getState())){
+            errorsList.add(Errors.builder().field(STATE).message(REQUIRED).build());
+        }
+        if(isEmpty(employeeRequest.getZipCode())){
+            errorsList.add(Errors.builder().field(ZIP_CODE).message(REQUIRED).build());
+        }
+        if(isEmpty(employeeRequest.getCountry())){
+            errorsList.add(Errors.builder().field(COUNTRY).message(REQUIRED).build());
+        }
+    }
+
+
+    private boolean isEmpty(String value){
+        return null == value;
+    }
+
+}
